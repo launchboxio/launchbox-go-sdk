1cfd742f1da4d092af1901aa87bf72589ea325c3
Robert Wittman <robkwittman@gmail.com>
2023-11-06 11:28:07 -0500

    Add GetKubeconfig to project SDK, return config errors, update README

--- config/config.go
+++ config/config.go
@@ -4,13 +4,22 @@ import "github.com/launchboxio/launchbox-go-sdk/launchbox"
 
 type Config struct {
 	Credentials *launchbox.Credentials
+
+	Endpoint string
 }
 
-func Default() *Config {
+func Default() (*Config, error) {
 	chain := launchbox.NewProviderChain()
 	credentials := chain.Resolve()
 
 	return &Config{
 		Credentials: credentials,
-	}
+		Endpoint:    "https://launchboxhq.io",
+	}, nil
+}
+
+func DefaultWithEndpoint(endpoint string) (*Config, error) {
+	cnf, err := Default()
+	cnf.Endpoint = endpoint
+	return cnf, err
 }
---
+++ service/project/operation_GetKubeconfig.go
@@ -0,0 +1,11 @@
+package project
+
+type GetKubeconfigInput struct {
+}
+
+type GetKubeconfigOutput struct {
+}
+
+func (c *Client) GetKubeconfig(input *GetKubeconfigInput) (*GetKubeconfigOutput, error) {
+	return nil, nil
+}
