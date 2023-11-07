56b1f507ca5b824b2941f35eaf1b74a3416c7509
Robert Wittman <robkwittman@gmail.com>
2023-11-07 15:32:27 -0500

    fix(clusters): Use PATCH /clusters instead of POST /ping

--- service/cluster/operation_Update.go
+++ service/cluster/operation_Update.go
@@ -1,11 +1,34 @@
 package cluster
 
+import "strconv"
+
 type UpdateClusterInput struct {
+	ClusterId       int    `json:"-"`
+	AgentVersion    string `json:"agent_version,omitempty"`
+	AgentIdentifier string `json:"agent_identifier,omitempty"`
+	Version         string `json:"version,omitempty"`
+	Provider        string `json:"provider,omitempty"`
+	Region          string `json:"region,omitempty"`
 }
 
 type UpdateClusterOutput struct {
+	Cluster Cluster `json:"cluster"`
 }
 
 func (c *Client) Update(input *UpdateClusterInput) (*UpdateClusterOutput, error) {
-	return nil, nil
+	payload := struct {
+		Cluster *UpdateClusterInput `json:"cluster"`
+	}{
+		Cluster: input,
+	}
+	result := &UpdateClusterOutput{}
+	client, err := c.cnf.GetClient()
+	if err != nil {
+		return nil, err
+	}
+	_, err = client.R().
+		SetResult(result).
+		SetBody(payload).
+		Patch("clusters/" + strconv.Itoa(input.ClusterId))
+	return result, err
 }
