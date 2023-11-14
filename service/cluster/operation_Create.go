c5a66e0f6404b2e3e426314ecc3e050c981cbb4c
Robert Wittman <robkwittman@gmail.com>
2023-11-14 10:43:25 -0500

    fix: Flesh our clusters.update and clusters.delete

--- service/cluster/operation_Create.go
+++ service/cluster/operation_Create.go
@@ -1,11 +1,28 @@
 package cluster
 
 type CreateClusterInput struct {
+	Domain string `json:"domain"`
+	Name   string `json:"name"`
 }
 
 type CreateClusterOutput struct {
+	Cluster *Cluster `json:"cluster"`
 }
 
 func (c *Client) Create(input *CreateClusterInput) (*CreateClusterOutput, error) {
-	return nil, nil
+	payload := struct {
+		Cluster *CreateClusterInput `json:"cluster"`
+	}{
+		Cluster: input,
+	}
+	result := &CreateClusterOutput{}
+	client, err := c.cnf.GetClient()
+	if err != nil {
+		return nil, err
+	}
+	_, err = client.R().
+		SetResult(result).
+		SetBody(payload).
+		Post("clusters")
+	return result, err
 }
--- service/cluster/operation_Delete.go
+++ service/cluster/operation_Delete.go
@@ -1,11 +1,22 @@
 package cluster
 
+import "strconv"
+
 type DeleteClusterInput struct {
+	ClusterId int
 }
 
 type DeleteClusterOutput struct {
 }
 
 func (c *Client) Delete(input *DeleteClusterInput) (*DeleteClusterOutput, error) {
-	return nil, nil
+	result := &DeleteClusterOutput{}
+	client, err := c.cnf.GetClient()
+	if err != nil {
+		return nil, err
+	}
+	_, err = client.R().
+		SetResult(result).
+		Delete("clusters/" + strconv.Itoa(input.ClusterId))
+	return result, err
 }
